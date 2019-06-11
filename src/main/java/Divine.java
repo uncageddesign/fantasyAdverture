26cd0ae3616e620820b3fc71662bba68695a04dc
mhairimccrindle@gmail.com <mhairimccrindle@gmail.com>
2019-06-11 23:16:42 +0100

    project initialised

---
+++ src/main/java/Divine.java
@@ -0,0 +1,2 @@
+public class Divine {
+}
---
+++ src/main/java/Player.java
@@ -0,0 +1,6 @@
+public class Player {
+    private String name;
+    private Integer hitPoints;
+    private Integer armourLevel;
+    private String alighnemt;
+}
